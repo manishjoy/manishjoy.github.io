d967d7c66e135829d1ff18b05f04daf234957405
Manish Joy <manishjoy1993@hotmail.com>
2019-05-11 13:50:28 +0530

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -18,4 +18,15 @@ jQuery(function ($) {
 
     $('.left-col-block, .right-col-block').theiaStickySidebar();
 
+    // --------------------------------------------------------------------
+    // For Mobile Titles
+    // --------------------------------------------------------------------
+
+    $("[title]").click(function(e) {
+        $(".title-tooltip").remove();
+        let titleWidth = $(this).attr("title").length * 7.1;
+        $(this).append(
+            '<span class="title-tooltip">' + $(this).attr("title") + "</span>"
+        );
+    });
 }); // JQuery end

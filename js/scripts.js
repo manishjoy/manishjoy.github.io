0a25b6724e28d7831e65c114d5f13413cb433a6e
Manish Joy <manishjoy1993@hotmail.com>
2019-10-17 02:17:14 +0530

    Changes for bottom container

--- js/scripts.js
+++ js/scripts.js
@@ -39,4 +39,60 @@ jQuery(function ($) {
             $(".title-tooltip").remove();
         }
     });
+
+    function amountscrolled(){
+        var winheight = $(window).height()
+        var docheight = $(document).height()
+        var scrollTop = $(window).scrollTop()
+        var trackLength = docheight - winheight
+        var pctScrolled = Math.floor(scrollTop/trackLength * 100) // gets percentage scrolled (ie: 80 NaN if tracklength == 0)
+        console.log(pctScrolled + '% scrolled')
+        return pctScrolled;
+    }
+     
+    $(window).on("scroll", function(){
+        var scrolledPct = amountscrolled();
+        if (scrolledPct <= 25) {
+            $('.bottom-fixed').removeClass('active');
+        } else {
+            var keepClosedToken = _accessCookie('keepclosed');
+            if (!keepClosedToken) {
+                $('.bottom-fixed').addClass('active');
+            }
+        }
+    });
+
+    function _createCookie(cookieName, value, minutes) {
+        if (minutes) {
+            var date = new Date();
+            date.setTime(date.getTime()+(minutes*60*1000));
+            var expires = "; expires = "+date.toGMTString();
+        } else {
+            var expires = "";
+        }
+        document.cookie = cookieName+"="+value+expires+"; path=/";
+    }
+
+    function _accessCookie(cookieName) {
+        var name = cookieName + '=';
+        var allCookieArray = document.cookie.split(';');
+
+        for(var i=0; i<allCookieArray.length; i++)
+        {
+            var temp = allCookieArray[i].trim();
+            if (temp.indexOf(name)==0)
+            return temp.substring(name.length,temp.length);
+        }
+        return '';
+    }
+
+    function _deleteCookie(cookieName) {
+        document.cookie = cookieName + '=; expires = Thu, 01 Jan 1970 00:00:01 GMT; path=/';
+    }
+
+    $(".bottom-fixed .close-btn, .bottom-fixed .close-link").click(function(e) {
+        e.preventDefault();
+        $('.bottom-fixed').removeClass('active');
+        _createCookie('keepclosed', 'yes', 2);
+    });
 }); // JQuery end
